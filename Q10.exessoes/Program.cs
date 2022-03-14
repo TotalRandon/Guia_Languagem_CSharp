372b4723cb437ab50fd78d38fa7164459d910c0a
João Victor de Castro Ferro <88172545+TotalRandon@users.noreply.github.com>
2022-03-14 10:50:35 -0300

    Add files via upload

---
+++ Q10.exessoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Q10.exessoes
+{
+    class ExcecaoAcimaDeCem : Exception
+    {
+        public void Excecao()
+        {
+            Console.WriteLine("O valor da soma nao valem 100");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, box = 0;
+
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Digite 5 numeros para somar ate 100");
+                    for(int i = 0; i < 5; i++)
+                    {
+                        a = float.Parse(Console.ReadLine());
+                        box = box + a;
+                    }
+                    if(box != 100) {  } else { return; }
+                }
+
+                catch(ExcecaoAcimaDeCem)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
